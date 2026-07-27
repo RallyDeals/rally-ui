7f457f10c6d471b8a20eb1dbf38dcb88ebb567af
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-07-27 07:57:07 +0300

    feat: add auto close on clicking outside mobile-menu

--- src/app/core/layout/header/header.ts
+++ src/app/core/layout/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { Logo } from '../../../shared/components/logo/logo';
 import { NgClass } from '@angular/common';
 import { DesktopNavLinks } from './desktop-nav-links/desktop-nav-links';
@@ -13,6 +13,8 @@ import { PrimaryBtn } from '../../../shared/components/buttons/primary-btn/prima
   styleUrl: './header.css',
 })
 export class Header {
+  constructor(private elementRef: ElementRef) {}
+
   menuVisible: boolean = false;
   selectedLink: string = 'home';
   loggedIn: boolean = false;
@@ -29,4 +31,19 @@ export class Header {
     this.selectedLink = link;
     this.menuVisible = false;
   };
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    if (!this.menuVisible) {
+      return;
+    }
+
+    const target = event.target as Node | null;
+    const mobileMenu = this.elementRef.nativeElement.querySelector('#mobile-menu') as HTMLElement | null;
+    const menuToggle = this.elementRef.nativeElement.querySelector('#menu-toggle') as HTMLElement | null;
+
+    if (target && ((mobileMenu && mobileMenu.contains(target)) ||
+        (menuToggle && menuToggle.contains(target)))) return;
+    this.menuVisible = false;
+  }
+
 }
