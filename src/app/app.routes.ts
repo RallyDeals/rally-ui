184ec3c2eae4cb1daa22afba30c93775a4f82ef4
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-07-27 07:36:38 +0300

    feat: add 2 layouts, one with header and footer and the other without for auth pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './core/layout/auth-layout/auth-layout';
+import { MainLayout } from './core/layout/main-layout/main-layout';
+import { Home } from './core/pages/home/home';
 import { AUTH_ROUTES } from './features/auth/auth.routes';
 
 
@@ -6,10 +9,21 @@ export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'auth/login',
+    redirectTo: 'home',
   },
   {
     path: 'auth',
+    component: AuthLayout,
     children: AUTH_ROUTES,
-  }
+  },
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: 'home',
+        component: Home
+      },
+    ],
+  },
 ];
