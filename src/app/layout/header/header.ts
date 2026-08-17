ff7b46a9a58b0823afe113caedae18098466f811
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-08-17 17:47:45 +0300

    chore: remove extra navlinks and delete unused component

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -22,8 +22,6 @@ export class Header {
     { name: 'Products', path: '/products', symbol: 'inventory_2' },
     { name: 'Deals', path: '/deals', symbol: 'local_offer' },
     { name: 'Categories', path: '/categories', symbol: 'category' },
-    { name: 'Seller Dashboard', path: '/seller', symbol: 'storefront' },
-    { name: 'Admin Dashboard', path: '/admin', symbol: 'admin_panel_settings' },
   ];
 
   toggleMenu() {
